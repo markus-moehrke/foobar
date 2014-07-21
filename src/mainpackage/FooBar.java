461fbee69073c99d02722be75ab2dd148e0e1a17
Markus Möhrke <markus.moehrke@gmail.com>
2014-07-21 20:40:18 +0200

    Ein bisschen Nonsense

--- src/mainpackage/FooBar.java
+++ src/mainpackage/FooBar.java
@@ -1,14 +1,25 @@
 package mainpackage;
 
-public class FooBar {
+public class FooBar implements IFoo {
 	
 	private String bla;
 	private String blubb;
 	
-	public FooBar() {}
+	public FooBar() {
+		System.out.println("FooBar: Thank you for instantiate me");
+	} 
 	
 	public FooBar(String bla, String blubb) {
+		System.out.println("FooBar: Thank you for instantiate me explicitly");
 		this.bla = bla;
 		this.blubb = blubb;
+	}
+
+	@Override
+	public void doNothing() {}
+
+	@Override
+	public String toString() {
+		return "I'm a stupid little instance of FooBar [bla=" + bla + ", blubb=" + blubb + "]";
 	}
 }
---
+++ src/mainpackage/FooFooBar.java
@@ -0,0 +1,8 @@
+package mainpackage;
+
+public class FooFooBar extends FooBar {
+
+	public FooFooBar() {
+		System.out.println("FooFooBar: Thank you for instantiate me");
+	} 
+}
